0e4b0379326646cfbc0d2182751bc51f1aacffbf
locazy <95532378+locazy@users.noreply.github.com>
2023-04-07 05:44:56 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
 
 import {ref} from 'vue'
-import {Expand, Search} from '@element-plus/icons-vue'
+import {ElementPlus, Expand, Search} from '@element-plus/icons-vue'
 
 const activeIndex = ref('1')
 const activeIndex2 = ref('1')
@@ -100,16 +100,66 @@ const carouseData = [
       </el-header>
       <!--      主页-->
       <el-main>
-        <div>
-          <!--        走马灯-->
-          <div class="block text-center">
-            <el-carousel height="550px" style="width: 60%; margin-left: 100px">
-              <el-carousel-item v-for="item in carouseData" :key="item">
-                <img :src="item.url" alt="" style="max-width: 100%; max-height: 100%; min-width: 100%; min-height: 100%"/>
-              </el-carousel-item>
-            </el-carousel>
+        <el-row gutter="20" style="height: 550px">
+          <el-col :span="16">
+            <div>
+<!--            走马灯-->
+            <div class="block text-center">
+                <el-carousel height="550px" style="margin-left: 100px">
+                <el-carousel-item v-for="item in carouseData" :key="item">
+                  <img :src="item.url" alt="" style="max-width: 100%; max-height: 100%; min-width: 100%; min-height: 100%"/>
+                </el-carousel-item>
+              </el-carousel>
+            </div>
           </div>
-        </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="upNext" style="margin-right: 100px">
+            <!--        即将上映-->
+            <span style="font-weight: bold; font-size: xx-large; color: #ffd04b"> 即将上映</span>
+              <div style="margin-top: 20px; flex-direction: column; margin-left: 30px">
+                <!--               <el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">&ndash;&gt;-->
+                <el-card :body-style="{ padding: '0px' }"  class="elcard" shadow="hover">
+                  <img src="/movie1.png" class="image">
+                  <div class="up-movie-block">
+                    <span style="font-weight: bold">'How Do You Know Your IMDb Page?' </span>
+                    <div class="bottom clearfix">
+                      <!--                          <time class="time">{{ currentDate }}</time>-->
+                      <span class="bfi">简介1:xxxxxxxxxxxxxxxxxxxx</span>
+                      <el-button type="text" class="button"></el-button>
+                    </div>
+                  </div>
+                </el-card>
+                <br>
+                <el-card :body-style="{ padding: '0px' }"  class="elcard" shadow="hover" >
+                  <img src="/movie2.png" class="image">
+                  <div class="up-movie-block">
+                    <span style="font-weight: bold">'Beau Is Afraid' </span>
+                    <div class="bottom clearfix">
+                      <!--                          <time class="time">{{ currentDate }}</time>-->
+                      <span class="bfi">简介2:xxxxxxxxxxxxxxxxxxxx</span>
+                      <el-button type="text" class="button"></el-button>
+                    </div>
+                  </div>
+                </el-card>
+                <br>
+                <el-card :body-style="{ padding: '0px' }"  class="elcard" shadow="hover"  >
+                  <img src="/movie3.png" class="image">
+                  <div class="up-movie-block">
+                    <span style="font-weight: bold">'Secret Invision' </span>
+                    <div class="bottom clearfix">
+                      <!--                          <time class="time">{{ currentDate }}</time>-->
+                      <span class="bfi">简介3:xxxxxxxxxxxxxxxxxxxx</span>
+                      <el-button type="text" class="button"></el-button>
+                    </div>
+                  </div>
+                </el-card>
+              </div>
+            </div>
+          </el-col>
+        </el-row>
+
+
       </el-main>
       <el-footer>Footer</el-footer>
     </el-container>
@@ -148,6 +198,61 @@ const carouseData = [
 
 .flex-grow {
   flex-grow: 0.6;
+}
+
+
+ .bfi {
+   font-size: 13px;
+   color: #999;
+ }
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  height: 40px;
+  width: 40px;
+  background-image: url("/button.png");
+  background-size: cover;
+  padding: 0;
+  float: right;
+}
+
+.image {
+  display: block;
+  width: 100px;
+  height: 150px;
+  object-fit: fill;
+  float: left;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both
+}
+
+.elcard{
+  height: 150px;
+  width: 450px;
+  box-shadow: white;
+  margin: auto;
+}
+
+.up-movie-block{
+  padding: 5px;
+  height: 120px;
+  width:330px;
+  float: right;
+  margin-right: 10px;
+  margin-top: 20px;
+  border-radius: 15px;
 }
 
 </style>
